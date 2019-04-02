dc838133176f293879a0dd005984d4873a283b82
chinjto <cyril_defaye@yahoo.fr>
2019-04-02 23:41:32 +0200

    Version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Post} from './post';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'sdz-blog';
+
+  posts: Post[] = [
+    {
+      title: 'Mon premmier post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+        'aliqua. Ut enim ad minim veniam, quis',
+      loveIts: 1,
+      createdAt: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+        'aliqua. Ut enim ad minim veniam, quis',
+      loveIts: -1,
+      createdAt: new Date()
+    },
+    {
+      title: 'Encore un post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+        'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+      loveIts: 0,
+      createdAt: new Date()
+    }
+  ];
+
 }
---
+++ src/app/post.ts
@@ -0,0 +1,8 @@
+export class Post {
+
+  title: string;
+  content: string;
+  loveIts: number;
+  createdAt: Date;
+
+}
